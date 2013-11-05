9c3aab62dfff3470fed0ac8c13321cbcaeffbb2e
noj33 <jonfriese33@gmail.com>
2013-11-05 15:08:14 -0800

    chapter 3

--- Gemfile
+++ Gemfile
@@ -1,2 +1,6 @@
 source 'https://rubygems.org'
-gem 'rulers', '~> 0.0.2'
+gem 'rulers', '~> 0.0.2', :path => "../rulers"
+
+group :development do
+  gem 'rerun'
+end
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,7 +3,7 @@ class QuotesController < Rulers::Controller
     "You miss 100% of the shots you don't take. -Wayne Gretzky" +
      "-Michael Scott" + "\n<pre>\n#{env}\n</pre>"
   end
-
+  #raise an exception
   def exception
     raise "It's a bad one!"
   end
