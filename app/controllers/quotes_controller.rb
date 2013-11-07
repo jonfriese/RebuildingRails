3d5627310c0f4cfcbbf5effcac760aa7e732374d
noj33 <jonfriese33@gmail.com>
2013-11-06 21:56:12 -0800

    chapter 5

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,9 +1,31 @@
 class QuotesController < Rulers::Controller
+
+  def index
+    quotes = FindModel.all
+    render :index, :quotes => quotes
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth a thousand pixels.",
+      "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
+
+
   def a_quote
     render :a_quote
   end
 
   def exception
     raise "It's a bad one!"
+  end
+
+  def quote_1
+    quote_1 = FileModel.find(1)
+    render :quote, :obj => quote_1
   end
 end
