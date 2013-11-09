99ecdd51e821b32a241522a94168e924eb5c13e2
noj33 <jonfriese33@gmail.com>
2013-11-08 21:37:36 -0800

    Chapter 7

---
+++ sqlite_test.rb
@@ -0,0 +1,13 @@
+require "sqlite3"
+require "rulers/sqlite_model"
+
+class MyTable < Rulers::Model::SQLite; end
+STDERR.puts MyTable.schema.inspect
+
+mt = MyTable.create "title" => "I saw it again!"
+mt["title"] = "I really did!"
+mt.save!
+
+mt2 = MyTable.find mt["id"]
+
+puts "Title: #{mt2["title"]}"
